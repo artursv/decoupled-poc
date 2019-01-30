50e98447e3692d02d7e66c6703538852ebd647ab
Arturs Vilkajs <arturs.vilkajs@wunder.io>
2019-01-30 23:19:43 +0200

    Working example of listing nodes from graphql query to Drupal.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,29 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDOM from 'react-dom'
+import './styles/index.css'
+import * as serviceWorker from './serviceWorker'
+import ApolloClient from 'apollo-client'
+import { ApolloProvider } from 'react-apollo'
+import { HttpLink } from 'apollo-boost'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import App from './components/App'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const cache = new InMemoryCache()
+
+const httpLink = new HttpLink({
+  uri: 'http://local.decoupled.com:8080/graphql',
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache,
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
