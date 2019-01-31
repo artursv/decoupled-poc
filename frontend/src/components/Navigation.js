0048c53cdae1975eb466af6b6bbe2bb4fc0b9417
Arturs Vilkajs <arturs.vilkajs@wunder.io>
2019-01-31 17:35:54 +0200

    Fetch main menu from Drupal and display in the nav component.

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -2,21 +2,45 @@ import React, { Component } from 'react';
 import Navbar from 'reactstrap/es/Navbar'
 import Nav from 'reactstrap/es/Nav'
 import NavLink from 'reactstrap/es/NavLink'
-import NavbarBrand from 'reactstrap/es/NavbarBrand'
+import { gql } from 'apollo-boost'
+import { Query } from 'react-apollo'
+
+const MENU_QUERY = gql`
+query {
+  menuByName(name: "main") {
+    links {
+      description
+      expanded
+      label
+      url {
+        path
+        routed
+      }
+    }
+  }
+}
+`
 
 class Navigation extends Component {
   render() {
     return(
-      <section>
-        <Navbar bg='dark'>
-          <NavbarBrand href="#home">Innovation day</NavbarBrand>
-          <Nav className="mr-auto">
-            <NavLink href="#home">Home</NavLink>
-            <NavLink href="#link">Link</NavLink>
-            <NavLink href="https://decoupled.lndo.site">Edit content</NavLink>
-          </Nav>
-        </Navbar>
-      </section>
+      <Query query={MENU_QUERY}>
+        {({ loading, error, data }) => {
+          if (loading) return 'Loading...';
+          if (error) return `Error! ${error}`;
+          console.log(data)
+          const linkList = data.menuByName.links.map((d) => <NavLink key={d.url.path} href={d.url.path}>{d.label}</NavLink>);
+          return (
+            <section>
+              <Navbar bg='dark'>
+                <Nav className="mr-auto">
+                  {linkList }
+                </Nav>
+              </Navbar>
+            </section>
+          )
+        }}
+      </Query>
     )
   }
 }
